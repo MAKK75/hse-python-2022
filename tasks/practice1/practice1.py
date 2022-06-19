3784c42b8ff1b3c5a0482b9f4ee8907760cf3750
MAKK75 <60113205+MAKK75@users.noreply.github.com>
2022-06-19 15:04:15 +0300

    practice1

--- tasks/practice1/practice1.py
+++ tasks/practice1/practice1.py
@@ -9,6 +9,7 @@ def concatenate_strings(a: str, b: str) -> str:
     """
 
     # пиши свой код здесь
+    result = a + b
 
     return result
 
@@ -22,6 +23,7 @@ def calculate_salary(total_compensation: int) -> float:
     :return: сумма заплаты после вычета налога
     """
 
-    # пиши свой код здесь
+
+    result = total_compensation * 0.87
 
     return result
