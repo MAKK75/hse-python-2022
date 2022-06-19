66276a62f897421794f91143045f5ab6cf2308ff
MAKK75 <60113205+MAKK75@users.noreply.github.com>
2022-06-19 17:51:30 +0300

    practice2

--- tasks/practice2/practice2.py
+++ tasks/practice2/practice2.py
@@ -1,4 +1,5 @@
 from typing import Iterable
+import random
 
 UNCULTURED_WORDS = ('kotleta', 'pirog')
 
@@ -13,6 +14,8 @@ def greet_user(name: str) -> str:
     """
 
     # пиши код здесь
+
+    greeting = "Hello " + name
     return greeting
 
 
@@ -29,6 +32,8 @@ def get_amount() -> float:
     """
 
     # пиши код здесь
+
+    amount = round(random.uniform(100, 100000), 2)
     return amount
 
 
@@ -43,6 +48,9 @@ def is_phone_correct(phone_number: str) -> bool:
     """
 
     # пиши код здесь
+
+    result = len(phone_number) == 12 and phone_number[0:2] == "+7" and phone_number[2:].isdigit()
+
     return result
 
 
@@ -59,6 +67,9 @@ def is_amount_correct(current_amount: float, transfer_amount: str) -> bool:
     """
 
     # пиши код здесь
+
+    result = current_amount >= float(transfer_amount)
+
     return result
 
 
@@ -78,6 +89,17 @@ def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
     """
 
     # пиши код здесь
+
+    text = text.strip()
+    text = text.capitalize()
+    text = text.replace("'", '')
+    text = text.replace('"', '')
+    for sl in UNCULTURED_WORDS:
+        if sl in text:
+            zamen = "#" * len(sl)
+            text = text.replace(sl, zamen)
+    result = text
+    
     return result
 
 
@@ -101,4 +123,9 @@ def create_request_for_loan(user_info: str) -> str:
     """
 
     # пиши код здесь
+
+    baza = user_info.split(',')
+    result = 'Фамилия: {}\nИмя: {}\nОтчество: {}\nДата рождения: {}\nЗапрошенная сумма: {}'.format(*baza)
+
+
     return result
